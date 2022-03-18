dfaf16f2fcba29ecd54fb7150633b65a0d2ddf5d
Fonghang <30502871+hysbeta@users.noreply.github.com>
2022-03-19 01:36:53 +0800

    Update jd_fcwb_help_template.js

--- jd_fcwb_help_template.js
+++ jd_fcwb_help_template.js
@@ -21,7 +21,7 @@ if ($.isNode()) {
     ].filter((item) => !!item);
 }
 let cookie = '';
-let curRound = 1!(async () => {
+!(async () => {
     if (!cookiesArr[0]) {
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {
             "open-url": "https://bean.m.jd.com/bean/signIndex.action"
